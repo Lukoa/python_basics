8e85da14e121a4aed2a42db5a49eeea12a74fce2
lukoa <wafula.carline19@students.dkut.ac.ke>
2023-03-25 12:59:52 +0300

    flask sample

--- log-in.py
+++ log-in.py
@@ -6,7 +6,7 @@ app= Flask(__name__)
 users={'carline': 'lukoa12', 'Akala': 'akala12'}
 
 #define the login route
-app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'username' in session:
         return redirect(url_for('dashboard'))
@@ -14,9 +14,28 @@ def login():
     if request.method =='POST':
         username=request.form['username']
         password=request.form['password']
-        if username in usera and users[username]==password:
+        if username in user and users[username]==password:
             session['username']=username
             return redirect(url_for('dashboard'))
         else:
             return render_template('login.html')
-app.route('/dashboard')    
+@app.route('/dashboard')
+def dashboard():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+        username=session['username']
+        return
+    render_template('dashboard.html', username=username)
+
+@app.route('/logout')
+def logout():
+    
+      
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+if __name__=='__main__':
+    app.secert_key='secret_key'
+    app.run(debug=True)
+
